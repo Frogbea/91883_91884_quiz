6de2af164dc669e204574e5ebb0995dafc69ee30
Em <86989285+Frogbea@users.noreply.github.com>
2022-08-05 14:47:30 +1200

    base_comp_v1
    this is the skeleton

---
+++ base_comp_quiz.py
@@ -0,0 +1,26 @@
+"""
+base component for a double digit multiplication quiz
+v1: skeleton []
+v2: comp 2 []
+v3: comp 3 []
+v4: comp 4 []
+v5: comp 5 []
+v6: comp 6 []
+v7: comp 7 []
+v8: final check
+author = E.G
+"""
+
+# imports
+
+
+# *****************functions****************************
+
+# random generator
+
+# answer checker
+
+# any other one I magically come up with
+
+# ******************************************************************************************
+# main route
