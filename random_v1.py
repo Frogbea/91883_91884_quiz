c676edd75f0d0095d73867b3146951f2a55ef2f8
Em <86989285+Frogbea@users.noreply.github.com>
2022-08-16 09:03:54 +1200

    random_v1
    generates 2 random numbers

---
+++ random_v1.py
@@ -0,0 +1,9 @@
+# make a generator that randomly multiplies 2 numbers
+# v1 making the generater
+
+import random as r
+
+num_1 = r.randint(10, 99)
+print(num_1)
+num_2 = r.randint(10, 99)
+print(num_2)
