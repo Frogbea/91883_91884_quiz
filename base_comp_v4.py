9cdf17e46c176d0da9f9283767967282740c96bc
Em <86989285+Frogbea@users.noreply.github.com>
2022-08-18 17:10:26 +1200

    base_quiz_v4_p2
    added in extra comp 7

--- base_comp_v4.py
+++ base_comp_v4.py
@@ -3,8 +3,8 @@ base component for a double digit multiplication quiz
 v1: skeleton [DONE]
 v2: comp 1 [DONE]
 v3: comp 2 [DONE]
-v4: comp 3 [DOING]
-v5: comp 4 [UNDONE]
+v4: comp 3 & 7[DONE]
+v5: comp 4 [DOING]
 v6: comp 5 [UNDONE]
 v7: comp 6 [UNDONE]
 v8: final check
@@ -31,7 +31,7 @@ def instructions():
             break
 
     # print instructions
-    print('Hello, this will be a double digit multiplication quiz. ')
+    print('Hello, this will be a multiplication quiz, you can choose the skill level, there is three options ')
     print('There will be 10 questions and at the end you will get a score out of ten. ')
     print('Please only answer with numbers. Good Luck')
 
@@ -50,11 +50,43 @@ def instructions():
             print('ERROR: please type yes or no')
 
 
+def player_level():
+    global num_min, num_max
+    while True:
+        try:
+            user_level = int(input('What level would you like to do? type in 1 for [1 - 10] | type in 2 for [10 - 20] | type in 3 for [10 - 99]:'))
+            if user_level == 1:
+                num_min = 1
+                num_max = 10
+                print('level one selected')
+                break
+            elif user_level == 2:
+                num_min = 10
+                num_max = 20
+                print('level two selected')
+                break
+            elif user_level == 3:
+                num_min = 10
+                num_max = 99
+                print('level three selected')
+                break
+            else:
+                print('Please input 1, 2 or 3')
+        except:
+            print('Error: please input the number ')
+
 # answer checker
 
 # any other one I magically come up with
 
 # ******************************************************************************************
+
+
+# variables
+num_min = 0
+num_max = 0
+
+
 # main route
 
 
@@ -73,12 +105,12 @@ while True:
     else:
         print('error: please type in yes or no')
 
+player_level()
+
 # repeats the question 10 times
 for n in range(10):
     # sets num_1 and num_2 as random numbers
-    num_1 = r.randint(10, 99)
-    num_2 = r.randint(10, 99)
+    num_1 = r.randint(num_min, num_max)
+    num_2 = r.randint(num_min, num_max)
     # asks the user to answer
     user_answer = int(input('What is {} x {}?'.format(num_1, num_2)))
-
-
